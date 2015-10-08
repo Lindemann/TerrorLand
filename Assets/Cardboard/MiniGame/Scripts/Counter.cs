c6eb0cde3c6aa5de3998ba36419e35feff35f999
Judith Lindemann <Lindemann@Judiths-MacBook-Pro.local>
2015-10-08 22:34:39 +0200

    init

---
+++ Assets/Cardboard/MiniGame/Scripts/Counter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+
+	int counter;
+	public Text textLeft;
+	public Text textRight;
+
+	void Start () {
+		counter = 0;
+		SetText (counter);
+	}
+
+	public void IncreaseCounter () {
+		counter++;
+		SetText (counter);
+	}
+
+	void SetText (int counter) {
+		textLeft.text = "Score: " + counter.ToString ();
+		textRight.text = "Score: " + counter.ToString ();
+	}
+}
---
+++ Assets/Cardboard/MiniGame/Scripts/GameOver.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		Destroy(other.gameObject);
+		StartGameOverScene();
+    }
+
+	void StartGameOverScene () {
+		Application.LoadLevel("GameOver");
+	}
+}
